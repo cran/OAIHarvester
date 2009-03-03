558480766683e44016700621301c37c57f02ca79
Kurt Hornik <Kurt.Hornik@R-project.org>
2009-03-03 00:00:00 +0000

    version 0.0-2

--- R/utils.R
+++ R/utils.R
@@ -22,6 +22,13 @@ function(x, names = NULL)
 {
     if(is.null(names))
         names <- unique(unlist(lapply(x, xmlApply, xmlName)))
+    ## <NOTE>
+    ## We used to eventually apply
+    ##    as.character(sapply(.elements_named(e, nm), xmlValue)))
+    ## but apparently xmlValue() gives character() rather than an empty
+    ## string for "empty" nodes ... eventually resulting in
+    ##    "character(0)"
+    ## entries.  Argh.  Let's use .xml_value_if_not_empty() instead.
     out <-
         do.call(cbind,
                 lapply(names,
@@ -30,7 +37,8 @@ function(x, names = NULL)
                                   function(e)
                                   as.character(sapply(.elements_named(e,
                                                                       nm),
-                                                      xmlValue)))
+                                                      .xml_value_if_not_empty))
+                                  )
                        }))
     dimnames(out) <- list(NULL, names)
     out
@@ -56,3 +64,7 @@ function(x, name)
 .xml_value_if_not_null <-
 function(n, default)
     if(!is.null(n)) xmlValue(n) else default
+
+.xml_value_if_not_empty <-
+function(n)
+    if(length(v <- xmlValue(n))) v else ""
